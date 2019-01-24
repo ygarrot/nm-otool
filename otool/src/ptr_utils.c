d4429c5420529f8e5c41823c296c1edfa5f3d208
ygarrot <garrot.yoan1@gmail.com>
2019-01-24 16:52:11 +0100

    #alanorm

--- otool/src/ptr_utils.c
+++ otool/src/ptr_utils.c
@@ -6,7 +6,7 @@
 /*   By: ygarrot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/30 11:34:41 by ygarrot           #+#    #+#             */
-/*   Updated: 2019/01/24 16:39:54 by ygarrot          ###   ########.fr       */
+/*   Updated: 2019/01/24 16:45:28 by ygarrot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,29 @@
 
 int		get_magic(void *ptr)
 {
-	int magic_number;
+	int	magic_number;
 
 	magic_number = *(int*)ptr;
-	return magic_number;
+	return (magic_number);
 }
 
 int		get_iter_nb(void *ptr, int type)
 {
-	t_otool *otool;
+	t_otool		*otool;
 
 	otool = get_otool(0);
 	if (type == LOAD_COMMAND)
 		return (otool->mem.iter_nb);
 	if (type == SECTION_64)
-		return (((t_segment_command_64*)(ptr - sizeof(t_segment_command_64)))->nsects);
+	{
+		return (((t_segment_command_64*)(ptr
+						- sizeof(t_segment_command_64)))->nsects);
+	}
 	if (type == SECTION)
-		return (((t_segment_command*)(ptr - sizeof(t_segment_command)))->nsects);
+	{
+		return (((t_segment_command*)(ptr
+						- sizeof(t_segment_command)))->nsects);
+	}
 	if (type == SYM_TAB_L)
 		return (UINT_MAX);
 	return (0);
@@ -38,6 +44,9 @@ int		get_iter_nb(void *ptr, int type)
 
 int		get_inc_value(void *ptr, int type)
 {
+	char	**str;
+	int		size;
+
 	if (type == LOAD_COMMAND)
 		return (((t_load_command*)ptr)->cmdsize);
 	if (type == SECTION_64)
@@ -46,10 +55,10 @@ int		get_inc_value(void *ptr, int type)
 		return (sizeof(t_section));
 	if (type == SYM_TAB_L)
 	{
-		char **str = ft_strsplit(ptr, ' ');
+		str = ft_strsplit(ptr, ' ');
 		if (ft_tablen(str) < 5)
 			return (INT_MAX);
-		int size = ft_atoi(str[5]);
+		size = ft_atoi(str[5]);
 		ft_free_dblechar_tab(str);
 		return (size + 60);
 	}
@@ -57,20 +66,21 @@ int		get_inc_value(void *ptr, int type)
 }
 
 void	iter_over_mem(void *child, void *struc, int type,
-		void	(*f)(void *elem, void *struc, uint32_t index))
+		void (*f)(void *elem, void *struc, uint32_t index))
 {
-	uint32_t							iter_nb;
-	uint32_t								i;
-	int											inc_value;
+	t_otool		*otool;
+	uint32_t	iter_nb;
+	uint32_t	i;
+	int			inc_value;
 
-	t_otool *otool = struc;
+	otool = struc;
 	i = -1;
 	iter_nb = get_int_indian(struc, get_iter_nb(child, type));
 	while (++i < iter_nb)
 	{
 		(*f)(child, struc, i);
 		inc_value = get_int_indian(struc, get_inc_value(child, type));
-		if (otool->error || otool->offset_handler(otool, child, inc_value)) 
+		if (otool->error || otool->offset_handler(otool, child, inc_value))
 		{
 			/* ft_printf("ABORT\n"); */
 			/* TODO : handle it lul */
--- otool/src/section.c
+++ otool/src/section.c
@@ -6,7 +6,7 @@
 /*   By: ygarrot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 13:25:38 by ygarrot           #+#    #+#             */
-/*   Updated: 2019/01/24 14:21:18 by ygarrot          ###   ########.fr       */
+/*   Updated: 2019/01/24 16:48:17 by ygarrot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,31 @@
 
 int		is_section_type(t_section *section, char *type)
 {
-		return (ft_strcmp(section->sectname, type)); 
+	return (ft_strcmp(section->sectname, type));
 }
 
-int		is_section64_type(t_section_64	*section64, char *type)
+int		is_section64_type(t_section_64 *section64, char *type)
 {
-		return (ft_strcmp(section64->sectname, type)); 
+	return (ft_strcmp(section64->sectname, type));
 }
 
 int		is_segment_type(t_section *section, char *type)
 {
-		return (ft_strcmp(section->segname, type)); 
+	return (ft_strcmp(section->segname, type));
 }
 
-int		is_segment64_type(t_section_64	*section64, char *type)
+int		is_segment64_type(t_section_64 *section64, char *type)
 {
-		return (ft_strcmp(section64->segname, type)); 
+	return (ft_strcmp(section64->segname, type));
 }
 
 int		is_text_sect(void *sect, unsigned int arch_type)
 {
 	if (arch_type == MH_MAGIC_64)
-		return (!is_section64_type(sect, SECT_TEXT) 
+	{
+		return (!is_section64_type(sect, SECT_TEXT)
 				&& !is_segment64_type(sect, SEG_TEXT));
-	return (!is_section_type(sect, SECT_TEXT) 
-				&& !is_segment_type(sect, SEG_TEXT));
+	}
+	return (!is_section_type(sect, SECT_TEXT)
+			&& !is_segment_type(sect, SEG_TEXT));
 }
-
